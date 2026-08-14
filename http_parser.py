5f0925c917a8258802d8f44d8a8fdc1bedd47d63
Sybqu <garrishtbhargava@gmail.com>
2026-08-15 04:21:44 +0530

    Add files via upload

---
+++ http_parser.py
@@ -0,0 +1,31 @@
+
+def http_handler(request : bytes) -> dict | None :
+    # print(f"data recieved = {data}")
+    if b"\r\n\r\n" not in request:
+            return None
+    req = request.split("\r\n")
+    meth , pth , ver = req[0].split()
+    print(f"{meth} , {pth} , {ver}")
+    
+    http_dict={}
+    http_dict["method"] = meth.strip()
+    http_dict["path"] = pth.strip()
+    http_dict["version"]=ver.strip()
+    headers_dict ={}
+
+   
+
+    for line in req[1:]:
+        if line=="":
+            break
+        header , value = line.split(b": ",maxsplit=1)
+        headers_dict[header.strip()]=value.strip()
+
+    http_dict["Headers"] = headers_dict
+    # print(http_dict)
+    # print(headers_dict)    
+    # print(req)
+    # print(f"method : {method}, path :{path} ,HTTP-Version : {version}")
+    
+    return http_dict
+
