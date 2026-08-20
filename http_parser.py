963147d6b29f14dbb4f4041115e0b72cb7a66ea4
Sybqu <garrishtbhargava@gmail.com>
2026-08-21 02:20:43 +0530

    Refactor http_handler for improved readability

--- http_parser.py
+++ http_parser.py
@@ -1,31 +1,57 @@
+fake_messages = ["Hello", "fake", "testing"]
 
-def http_handler(request : bytes) -> dict | None :
-    # print(f"data recieved = {data}")
+
+def http_handler(request: bytes) -> dict | None:
     if b"\r\n\r\n" not in request:
-            return None
-    req = request.split("\r\n")
-    meth , pth , ver = req[0].split()
+        return None
+
+    req = request.split(b"\r\n")
+    meth, pth, ver = req[0].split()
     print(f"{meth} , {pth} , {ver}")
-    
-    http_dict={}
+
+    http_dict = {}
     http_dict["method"] = meth.strip()
     http_dict["path"] = pth.strip()
-    http_dict["version"]=ver.strip()
-    headers_dict ={}
+    http_dict["version"] = ver.strip()
+    headers_dict = {}
 
-   
-
+    # header parsing
     for line in req[1:]:
-        if line=="":
+        if line == b"":
             break
-        header , value = line.split(b": ",maxsplit=1)
-        headers_dict[header.strip()]=value.strip()
+        header, value = line.split(b": ", maxsplit=1)
+        headers_dict[header.strip()] = value.strip()
 
-    http_dict["Headers"] = headers_dict
-    # print(http_dict)
-    # print(headers_dict)    
-    # print(req)
-    # print(f"method : {method}, path :{path} ,HTTP-Version : {version}")
-    
+    http_dict["headers"] = headers_dict
+
+    # body parsing (POST)
+    if http_dict["method"] == b"POST":
+        if b"Content-Length" in http_dict["headers"]:
+            body_len = int(http_dict["headers"][b"Content-Length"])
+            _, body_post = request.split(b"\r\n\r\n", 1)
+
+            if len(body_post) < body_len:
+                return None  # body hasn't fully arrived yet — wait for more recv()
+
+            http_dict["body"] = body_post[:body_len]
+
     return http_dict
 
+
+def handle_index(req):
+    with open("index.html", "rb") as f:
+        body = f.read()
+    return 200, {"Content-Type": "text/html"}, body
+
+
+def handle_get_messages(req):
+    body = "\n".join(fake_messages).encode()
+    return 200, {"Content-Type": "text/plain"}, body
+
+
+def handle_post_messages(req):
+    fake_messages.append(req["body"].decode())
+    return 201, {}, b"message received"
+
+def handle_404(req):
+    return 404, {"Content-Type": "text/plain"}, b"not found"
